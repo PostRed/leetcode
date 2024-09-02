f0df27b85c075979b39ccf5aa1835cd39dfcde21
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-09-02 18:38:40 +0300

    Update main.py

--- bin_search/search_in_2d_matrix/main.py
+++ bin_search/search_in_2d_matrix/main.py
@@ -1,17 +1,15 @@
-from typing import List
-
-
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        left = 0
-        right = len(matrix) * len(matrix[0]) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            num_mid = matrix[mid // len(matrix)][mid % len(matrix)]
-            if num_mid == target:
-                return True
-            elif num_mid < target:
-                left = mid + 1
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        l = 0
+        r = len(matrix) * len(matrix[0]) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            y_mid = mid // len(matrix[0])
+            x_mid = mid % len(matrix[0])
+            if matrix[y_mid][x_mid] < target:
+                l = mid + 1
+            elif matrix[y_mid][x_mid] > target:
+                r = mid - 1
             else:
-                right = mid - 1
+                return True
         return False
