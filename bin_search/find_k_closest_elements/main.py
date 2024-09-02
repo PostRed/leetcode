39f42d75bcc1a76aad588e2e119592501503ed3e
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-09-02 18:51:24 +0300

    Update main.py

--- bin_search/find_k_closest_elements/main.py
+++ bin_search/find_k_closest_elements/main.py
@@ -1,25 +1,14 @@
-from typing import List
-
-
-class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        return arr.sort(key=lambda i: abs(x - i))[:k]
-
-    # bin search
-
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        # use two mid pointes method
-        left, right = 0, len(arr) - k
-
-        while left < right:
-            mid = (left + right) // 2
-            # mid + k must not out of range
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        l = 0
+        r = len(arr) - k
+        while l < r:
+            mid = (l + r) // 2
+            print(l, r)
             if abs(arr[mid] - x) > abs(arr[mid + k] - x):
-                left = mid + 1
-            # if All duplicate and the first one is large than x
-            # arr[mid] < x need to filter out this kind of case: [1,1,2,2,2,2,2,2,2,2] 3 1
+                l = mid + 1
             elif arr[mid] == arr[mid + k] and arr[mid] < x:
-                left = mid + 1
+                l = mid + 1
             else:
-                right = mid
-        return arr[left:left + k]
+                r = mid
+        return arr[l:l + k]
