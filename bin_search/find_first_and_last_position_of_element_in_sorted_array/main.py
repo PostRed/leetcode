cddfe453a79868a85fb0b4d353b4731968796da8
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-09-03 21:05:00 +0300

    Update main.py

--- bin_search/find_first_and_last_position_of_element_in_sorted_array/main.py
+++ bin_search/find_first_and_last_position_of_element_in_sorted_array/main.py
@@ -1,40 +1,33 @@
-from typing import List, Tuple, Any
-
-
-class Solution:
-
-    def leftBinSearch(self, nums, target):
-        left = 0
-        right = len(nums) - 1
-        ans = -1
-        while left <= right:
-            mid = (left + right) // 2
+class Solution(object):
+    def lSearch(self, nums, target):
+        l = 0
+        r = len(nums) - 1
+        res = -1
+        while l <= r:
+            mid = (l + r) // 2
             if nums[mid] < target:
-                left = mid + 1
+                l = mid + 1
             elif nums[mid] > target:
-                right = mid - 1
+                r = mid - 1
             else:
-                ans = mid
-                right = mid - 1
-        return ans
+                res = mid
+                r = mid - 1
+        return res
 
-    def rightBinSearch(self, nums, target):
-        left = 0
-        right = len(nums) - 1
-        ans = -1
-        while left <= right:
-            mid = (left + right) // 2
+    def rSearch(self, nums, target):
+        l = 0
+        r = len(nums) - 1
+        res = -1
+        while l <= r:
+            mid = (l + r) // 2
             if nums[mid] < target:
-                left = mid + 1
+                l = mid + 1
             elif nums[mid] > target:
-                right = mid - 1
+                r = mid - 1
             else:
-                ans = mid
-                left = mid + 1
-        return ans
+                res = mid
+                l = mid + 1
+        return res
 
-    def searchRange(self, nums: List[int], target: int):
-        return self.leftBinSearch(nums, target), self.rightBinSearch(nums, target)
-
-nums = [5,7,7,8,8,10]
-print(Solution().leftBinSearch(nums, 8))
+    def searchRange(self, nums, target):
+        return [self.lSearch(nums, target), self.rSearch(nums, target)]
