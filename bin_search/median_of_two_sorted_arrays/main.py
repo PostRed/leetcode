d7fe4ade6b877b3ec35e4e872782252d8cfc0140
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-09-04 17:44:38 +0300

    Update main.py

--- bin_search/median_of_two_sorted_arrays/main.py
+++ bin_search/median_of_two_sorted_arrays/main.py
@@ -1,47 +1,37 @@
-from typing import List
-
-
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # nums1 должно быть меньше, так как по нему будем делать бинпоиск
-        if len(nums1) >= len(nums2):
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
-        n, m = len(nums1), len(nums2)
-        total_len = n + m
-        half = total_len // 2
 
-        # бинпоиск по меньшему массиву
-        left, right = 0, n - 1
-        while True:
+        n1 = len(nums1)
+        n2 = len(nums2)
+
+        left = 0
+        right = n1
+        total = n1 + n2
+        half_len = (total + 1) // 2
+
+        while left <= right:
+            # нужно построить линию так, чтобы все элементы слева были одинаковы и меньше всех (одинаковых) элементов справа
             mid1 = (left + right) // 2
-            mid2 = half - mid1 - 2
-            # теперь найдем 4 значения возле каждой серединки
-            if mid1 >= 0:
-                left_by_mid1 = nums1[mid1]
-            else:
-                left_by_mid1 = float("-inf")
-            if mid1 < n - 1:
-                right_by_mid1 = nums1[mid1 + 1]
-            else:
-                right_by_mid1 = float("+inf")
-            if mid2 >= 0:
-                left_by_mid2 = nums2[mid2]
-            else:
-                left_by_mid2 = float("-inf")
-            if mid2 < m - 1:
-                right_by_mid2 = nums2[mid2 + 1]
-            else:
-                right_by_mid2 = float("+inf")
-            if left_by_mid1 <= right_by_mid2 and left_by_mid2 <= right_by_mid1:
-                if total_len % 2 == 0:
-                    return (max(left_by_mid1, left_by_mid2) + min(right_by_mid1, right_by_mid2)) / 2
-                else:
-                    return min(right_by_mid1, right_by_mid2)
-            else:
-                if left_by_mid1 > right_by_mid2:
-                    right = mid1 - 1
-                else:
-                    left = mid1 + 1
+            mid2 = half_len - mid1
 
+            l1 = nums1[mid1 - 1] if mid1 > 0 else -1e9
+            r1 = nums1[mid1] if mid1 < n1 else 1e9
 
-print(Solution().findMedianSortedArrays([], [1]))
+            l2 = nums2[mid2 - 1] if mid2 > 0 else -1e9
+            r2 = nums2[mid2] if mid2 < n2 else 1e9
+
+            # Если наше условие соблюдено
+            if l1 <= r2 and l2 <= r1:
+                if total % 2 == 0:
+                    return (max(l1, l2) + min(r1, r2)) / 2.0
+                else:
+                    return max(l1, l2)
+
+            elif l1 > r2:
+                # то линия левее
+                right = mid1 - 1
+            else:
+                # то линия правее
+                left = mid1 + 1
