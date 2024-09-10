1ab95efac97f52d4c1e742031eb1c05a5225d45d
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-09-10 20:41:29 +0300

    Update main.py

--- binary_tree/lowest_common_ancestor_of_a_binary_search_tree/main.py
+++ binary_tree/lowest_common_ancestor_of_a_binary_search_tree/main.py
@@ -1,12 +1,5 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def lowestCommonAncestor(self, root, p, q):
         cur = root
         if p.val > q.val:
             p, q = q, p
@@ -17,16 +10,3 @@ class Solution:
                 cur = cur.left
             else:
                 cur = cur.right
-
-    def lowestCommonAncestor2(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root == p or root == q or not root:
-            return root
-        l = self.lowestCommonAncestor(root.left, p, q)
-        r = self.lowestCommonAncestor(root.right, p, q)
-
-        if l and r:
-            return root
-        elif l:
-            return l
-        elif r:
-            return r
