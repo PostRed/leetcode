b899f403bce0addd3ec1cb6fbb257db68d61789e
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-09-23 15:35:18 +0300

    Update main.py

--- string/longest_palindromic_substring/main.py
+++ string/longest_palindromic_substring/main.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution(object):
 
     def palindrome_expand(self, left, right, s):
         while left >= 0 and right < len(s) and s[left] == s[right]:
@@ -6,10 +6,13 @@ class Solution:
             right += 1
         return s[left + 1:right]
 
-    def longestPalindrome(self, s: str) -> str:
+    def longestPalindrome(self, s):
         ans = ''
         for i in range(len(s)):
+            # Возвращает палинтромную подстроку нечетной длины
             pal_odd = self.palindrome_expand(i, i, s)
-            pal_even = self.palindrome_expand(i, i + 1, s)
+            # Возвращает палинтромную подстроку четной длины
+            pal_even =  self.palindrome_expand(i, i + 1, s)
+            print(pal_odd)
             ans = max([ans, pal_odd, pal_even], key=len)
         return ans
