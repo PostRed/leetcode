2072415b8d86222cac36ae6875da5adf86d60fca
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-09-23 15:24:33 +0300

    Update main.py

--- string/isomorphic_strings/main.py
+++ string/isomorphic_strings/main.py
@@ -1,18 +1,15 @@
 class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
+    def isIsomorphic(self, s, t):
         if len(s) != len(t):
             return False
-        dict1, dict2 = {}, {}
+        # В словарях храним замены букв для каждой из строк
+        dict_s, dict_t = {}, {}
         for i in range(len(s)):
-            if s[i] in dict1:
-                if t[i] != dict1[s[i]]:
-                    return False
+            if s[i] in dict_s and t[i] != dict_s[s[i]]:
+                return False
+            elif s[i] not in dict_s and t[i] in dict_t:
+                return False
             else:
-                if t[i] in dict2:
-                    return False
-                dict1[s[i]] = t[i]
-                dict2[t[i]] = s[i]
+                dict_s[s[i]] = t[i]
+                dict_t[t[i]] = s[i]
         return True
-
-
-Solution().isIsomorphic("egg", "add")
