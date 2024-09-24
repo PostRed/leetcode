ccf656539b3ffff2756edfbfb85629022fb52428
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-09-24 15:44:31 +0300

    Update main.py

--- string/longest_repeating_character_replacement/main.py
+++ string/longest_repeating_character_replacement/main.py
@@ -12,8 +12,10 @@ class Solution(object):
                 hist[s[right]] += 1
             else:
                 hist[s[right]] = 1
-
+            # Обновляем максимальное количество одинаковых символов в окне
             max_len = max(max_len, hist[s[right]])
+            # Если длина окна минус максимальное количество одинаковых символов
+            # превышает k, значит нужно сдвинуть левую границу окна
             if right - left + 1 - max_len > k:
                 hist[s[left]] -= 1
                 left += 1
