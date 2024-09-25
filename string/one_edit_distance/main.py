0e76df9c3c093e5672d33f28cd7a604ec9f96c49
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-09-25 16:54:44 +0300

    Update main.py

--- string/one_edit_distance/main.py
+++ string/one_edit_distance/main.py
@@ -1,5 +1,5 @@
 class Solution:
-    def is_one_edit_distance(self, s: str, t: str) -> bool:
+    def is_one_edit_distance(self, s, t):
         if len(s) == len(t):
             count_of_differences = 0
             for i in range(len(s)):
@@ -8,12 +8,12 @@ class Solution:
             return count_of_differences == 1
         if len(s) == len(t) + 1:
             for i in range(len(s)):
+                # t = s без одного элемента
                 if t == s[0:i] + s[i + 1:]:
                     return True
         if len(s) == len(t) - 1:
             for i in range(len(t)):
+                # s = t без одного элемента
                 if s == t[0:i] + t[i + 1:]:
                     return True
         return False
-
-print(Solution().is_one_edit_distance("ab", "ab"))
