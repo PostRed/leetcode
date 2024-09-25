994aec2b2ccb5846dfb1fcfecb85ad82b7ecfdd9
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-09-25 17:04:23 +0300

    Update main.py

--- string/partition_labels/main.py
+++ string/partition_labels/main.py
@@ -1,15 +1,14 @@
-from typing import List
-
-
 class Solution:
-    def partitionLabels(self, s: str) -> List[int]:
+    def partitionLabels(self, s):
         last = {}
         for i in range(len(s)):
             last[s[i]] = i
         left = right = 0
         ans = []
         for i in range(len(s)):
+            # правая граница - это самое дальнее вхождение
             right = max(right, last[s[i]])
+            # если правее нет, то мы нашли нужный отрезок 
             if i == right:
                 ans.append(right - left + 1)
                 left = i + 1
