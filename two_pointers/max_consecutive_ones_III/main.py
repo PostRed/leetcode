3b421db42c0578edf1795aaaf8d2060f94aac8c2
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-01 15:25:14 +0300

    Update main.py

--- two_pointers/max_consecutive_ones_III/main.py
+++ two_pointers/max_consecutive_ones_III/main.py
@@ -1,17 +1,13 @@
-from typing import List
-
-
 class Solution:
-    def longestOnes(self, nums: List[int], k: int) -> int:
-        left = right = 0
+    def longestOnes(self, nums, k):
+        left = 0
+        ans = 0
         for right in range(len(nums)):
             if nums[right] == 0:
                 k -= 1
-            if k < 0:
+            while k < 0:
                 if nums[left] == 0:
                     k += 1
                 left += 1
-        return right - left + 1
-
-nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
-print(Solution().longestOnes(nums, 3))
+            ans = max(ans, right - left + 1)
+        return ans
