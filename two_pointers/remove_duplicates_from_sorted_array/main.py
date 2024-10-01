24c43033d147151d168d644a34d37bb717fb17b0
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-01 15:31:10 +0300

    Update main.py

--- two_pointers/remove_duplicates_from_sorted_array/main.py
+++ two_pointers/remove_duplicates_from_sorted_array/main.py
@@ -1,11 +1,10 @@
-from typing import List
-
-
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        j = 1
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                nums[j] = nums[i]
-                j += 1
-        return j
+    def removeDuplicates(self, nums):
+        l = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r - 1]:
+                # l делаем уникальным элементом
+                nums[l] = nums[r]
+                l += 1
+        # позиция последнего уникального элемента
+        return l
