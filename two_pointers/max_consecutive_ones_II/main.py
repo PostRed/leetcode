a619ce70343435772236f22daf3b7f0991896c2e
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-01 15:18:21 +0300

    Update main.py

--- two_pointers/max_consecutive_ones_II/main.py
+++ two_pointers/max_consecutive_ones_II/main.py
@@ -1,16 +1,11 @@
-from typing import (
-    List,
-)
-
-
 class Solution:
-    def find_max_consecutive_ones(self, nums: List[int]) -> int:
+    def find_max_consecutive_ones(self, nums):
         ans = 0
         zeros = 0
 
         l = 0
-        for r, num in enumerate(nums):
-            if num == 0:
+        for r in range(len(nums)):
+            if nums[r]== 0:
                 zeros += 1
             while zeros == 2:
                 if nums[l] == 0:
@@ -18,6 +13,3 @@ class Solution:
                 l += 1
             ans = max(ans, r - l + 1)
         return ans
-
-
-print(Solution().find_max_consecutive_ones([1, 0, 1, 1, 0]))
