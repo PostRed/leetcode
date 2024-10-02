2b21e0522df93cc3a2d0a30529541f2ebf943d0d
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-02 14:11:18 +0300

    Update main.py

--- two_pointers/product_of_two_run_length_encoded_arrays/main.py
+++ two_pointers/product_of_two_run_length_encoded_arrays/main.py
@@ -1,20 +1,22 @@
-from typing import List
-
-
 class Solution:
-    def findRLEArray(self, encode1: List[List[int]], encode2: List[List[int]]) -> List[List[int]]:
+    def findRLEArray(self, encoded1, encoded2):
         ans = []
         j = 0
-        for val_i, freq_i in encode1:
+        # значение, число повторений
+        for val_i, freq_i in encoded1:
             while freq_i:
-                freq_cur = min(freq_i, encode2[j][1])
-                val_cur = val_i * encode2[j][0]
+                # минимальное число повторений элемента
+                freq_cur = min(freq_i, encoded2[j][1])
+                val_cur = val_i * encoded2[j][0]
+
                 if ans and ans[-1][0] == val_cur:
                     ans[-1][1] += freq_cur
                 else:
                     ans.append([val_cur, freq_cur])
+
                 freq_i -= freq_cur
-                encode2[j][1] -= freq_cur
-                if not encode2[j][1]:
+                encoded2[j][1] -= freq_cur
+                # если исчерпали повторы во втором списке - в нем переходи дальше
+                if not encoded2[j][1]:
                     j += 1
         return ans
