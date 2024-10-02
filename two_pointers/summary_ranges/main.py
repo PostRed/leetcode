b3ed1d4a0c4f23be37b5ade3e7ac925569e994e1
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-02 16:36:26 +0300

    Update main.py

--- two_pointers/summary_ranges/main.py
+++ two_pointers/summary_ranges/main.py
@@ -1,20 +1,26 @@
 class Solution(object):
     def summaryRanges(self, nums):
-        ptr1 = 0
-        ptr2 = 1
+        left = 0
+        right = 1
         if len(nums) < 2:
             return list(map(str, nums))
+        
         ans = []
-        while ptr2 < len(nums):
-            if nums[ptr2 - 1] != nums[ptr2] - 1:
-                if ptr1 == ptr2 - 1:
-                    ans.append(str(ptr1))
+        while right < len(nums):
+            # упорядоченная последовательность кончилась
+            if nums[right - 1] != nums[right] - 1:
+                # если только один элемент
+                if left == right - 1:
+                    ans.append(str(nums[left]))
                 else:
-                    ans.append(str(nums[ptr1]) + '->' + str(nums[ptr2 - 1]))
-                ptr1 = ptr2
-            ptr2 += 1
-        if ptr1 == ptr2 - 1:
-            ans.append(str(ptr1))
+                    ans.append(str(nums[left]) + '->' + str(nums[right - 1]))
+                # идем искать новое окно
+                left = right
+                
+            right += 1
+            
+        if left == right - 1:
+            ans.append(str(nums[left]))
         else:
-            ans.append(str(nums[ptr1]) + '->' + str(nums[ptr2 - 1]))
+            ans.append(str(nums[left]) + '->' + str(nums[right - 1]))
         return ans
