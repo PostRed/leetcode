727fff9c02f77909dbc1033d5c4c121f99f86723
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-03 16:16:17 +0300

    Update main.py

--- two_pointers/two_sum_||_input_array_is_sorted/main.py
+++ two_pointers/two_sum_||_input_array_is_sorted/main.py
@@ -1,14 +1,17 @@
-from typing import List
-
-
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        i = 0
-        j = len(numbers) - 1
-        while i < j:
-            if numbers[i] + numbers[j] == target:
-                return [i, j]
-            if numbers[i] + numbers[j] < target:
-                i += 1
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left, right]
+            # так как список отсортирован, если сумма меньше - ее надо увеличить с помощью передвижения l
+            if numbers[left] + numbers[right] < target:
+                left += 1
             else:
-                j -= 1
+                right -= 1
