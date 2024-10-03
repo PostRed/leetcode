9777b2cb92090a9a67fa644d301b89098237f705
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-03 16:30:53 +0300

    Update main.py

--- other/find_the_prefix_common_array_of_two_arrays/main.py
+++ other/find_the_prefix_common_array_of_two_arrays/main.py
@@ -1,20 +1,20 @@
-from typing import List
+class Solution(object):
+    def findThePrefixCommonArray(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        n = len(A)
+        res = [0] * n
+        count_A = set() 
+        count_B = set()
 
+        for i in range(n):
+            count_A.add(A[i]) 
+            count_B.add(B[i])
 
-class Solution:
-    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        hist_a = [0] * (len(A) + 1)
-        hist_b = [0] * (len(A) + 1)
-        ans = [0] * (len(A) + 1)
-        for i in range(len(A)):
-            hist_a[A[i]] += 1
-            hist_b[B[i]] += 1
-            ans[i + 1] = ans[i]
-            if A[i] == B[i]:
-                ans[i + 1] += 1
-            else:
-                if hist_a[A[i]] == hist_b[A[i]]:
-                    ans[i + 1] += 1
-                if hist_a[B[i]] == hist_b[B[i]]:
-                    ans[i + 1] += 1
-        return ans[1:]
+            # Количество общих элементов на индексе i
+            res[i] = len(count_A.intersection(count_B))
+
+        return res
