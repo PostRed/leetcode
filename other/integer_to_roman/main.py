b4a287e1357b3e34688e901108910d7b976fab2c
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-04 15:33:01 +0300

    Update main.py

--- other/integer_to_roman/main.py
+++ other/integer_to_roman/main.py
@@ -1,40 +1,28 @@
-class Solution:
-    def intToRoman(self, num: int) -> str:
-        ans = ""
-        thousands = num // 1000
-        ans += thousands * "M"
-        num %= 1000
-        if num >= 900:
-            ans += "CM"
-            num -= 900
-        if num >= 500:
-            ans += "D"
-            num -= 500
-        if num >= 400:
-            ans += "CD"
-            num -= 400
-        hundreds = num // 100
-        ans += hundreds * "C"
-        num %= 100
-        if num >= 90:
-            ans += "XC"
-            num -= 90
-        if num >= 50:
-            ans += "L"
-            num -= 50
-        if num >= 40:
-            ans += "XL"
-            num -= 40
-        tenth = num // 10
-        ans += tenth * "X"
-        num %= 10
-        if num == 9:
-            ans += "IX"
-            return ans
-        if num >= 5:
-            ans += "V"
-            num -= 5
-        if num == 4:
-            ans += "IV"
-            return ans
-        return ans + num * "I"
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type vals: int
+        :rtype: str
+        """
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+        ]
+        syms = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+        ]
+
+        roman_numeral = ""
+        i = 0
+        while num > 0:
+            # Добавляем каждое значение нужно количество раз
+            for _ in range(num // val[i]):
+                roman_numeral += syms[i]
+                num -= val[i]
+            i += 1
+        return roman_numeral
