c06e52313aaef089804f7ecd8d131ea404a2309e
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-07 14:17:33 +0300

    Update main.py

--- other/interval_list_intersections/main.py
+++ other/interval_list_intersections/main.py
@@ -1,27 +1,24 @@
-from typing import List
-
-
 class Solution:
     def intersection(self, i1, i2):
+        # большее из начал
         low = max(i1[0], i2[0])
+        # меньшее из концов
         high = min(i1[1], i2[1])
         if low > high:
             return [high]
         return [low, high]
 
-    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        i = j = 0
+    def intervalIntersection(self, firstList, secondList):
+        p1 = p2 = 0
         ans = []
-        while i < len(firstList) and j < len(secondList):
-            inter = self.intersection(firstList[i], secondList[j])
+        while p1 < len(firstList) and p2 < len(secondList):
+            inter = self.intersection(firstList[p1], secondList[p2])
             if len(inter) > 1:
                 ans.append(inter)
-            if secondList[j][1] > inter[-1]:
-                i += 1
+            #  Если конец второго больше, чем конец пересечения - сдвигаем указатель в первом
+            if secondList[p2][1] > inter[-1]:
+                p1 += 1
+            #  Если конец второго меньше, чем конец пересечения - сдвигаем указатель во втором
             else:
-                j += 1
+                p2 += 1
         return ans
-
-first = [[0,2],[5,10],[13,23],[24,25]]
-sec = [[1,5],[8,12],[15,24],[25,26]]
-print(Solution().intervalIntersection(first, sec))
