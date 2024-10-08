cc5c7d861368a12414747cbe80a2c1b5769f4a34
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-08 14:48:33 +0300

    Update main.py

--- other/merge_intervals/main.py
+++ other/merge_intervals/main.py
@@ -6,7 +6,9 @@ class Solution:
         intervals = sorted(intervals)
         merged = [intervals[0]]
         for i in range(1, len(intervals)):
+            # если текущий интервал начинается не позже конца последнего из списка
             if intervals[i][0] <= merged[-1][1]:
+                # последний интервал делаем длинейшим 
                 merged[-1][1] = max(intervals[i][1], merged[-1][1])
             else:
                 merged.append(intervals[i])
