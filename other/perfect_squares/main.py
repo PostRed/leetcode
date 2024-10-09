75f371895fe7ab1d1aaa4ef0454860a2993207c9
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-09 17:17:50 +0300

    Update main.py

--- other/perfect_squares/main.py
+++ other/perfect_squares/main.py
@@ -1,12 +1,10 @@
-import math
-
-
 class Solution:
-    def numSquares(self, n: int) -> int:
-        dp = [i for i in range(n + 1)]
+    def numSquares(self, n):
+        # хранение минимального количества квадратов для каждого числа от 0 до n
+        dp = [1e9] * (n + 1)
+        dp[0] = 0
         for i in range(n + 1):
+            # для каждого числа перебираем все совершенные квадраты
             for j in range(1, int(i ** 0.5) + 1):
                 dp[i] = min(dp[i], dp[i - j ** 2] + 1)
         return dp[n]
-
-print(Solution().numSquares(13))
