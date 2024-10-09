465a397708517bf036fb1442584de3c137144e4d
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-09 17:01:18 +0300

    Update main.py

--- other/merge_sorted_array/main.py
+++ other/merge_sorted_array/main.py
@@ -1,17 +1,14 @@
-from typing import List
-
-
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        i = m - 1
-        j = n - 1
-        k = m + n - 1
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                k -= 1
-                i -= 1
+    def merge(self, nums1, m, nums2, n) -> None:
+        i1 = m - 1
+        i2 = n - 1
+        current = m + n - 1
+        while i2 >= 0:
+            # с конца записываем бОльший из элементов
+            if i1 >= 0 and nums1[i1] > nums2[i2]:
+                nums1[current] = nums1[i1]
+                i1 -= 1
             else:
-                nums1[k] = nums2[j]
-                k -= 1
-                j -= 1
+                nums1[current] = nums2[i2]
+                i2 -= 1
+            current -= 1
