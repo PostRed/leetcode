82ecf67d36cef52808200bd82338cce2bc337d19
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-14 21:00:36 +0300

    Update main.py

--- other/two_sum/main.py
+++ other/two_sum/main.py
@@ -5,6 +5,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         indices = {}
         for i in range(len(nums)):
+            # если в списке есть число, которое в сумме с текущем даст цель
             if target - nums[i] in indices:
                 return [indices[target - nums[i]], i]
             indices[nums[i]] = i
