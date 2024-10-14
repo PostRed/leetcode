244436156da1636f817027ffc99b13ae913ebe30
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-14 15:58:37 +0300

    Update main.py

--- other/rotate_image/main.py
+++ other/rotate_image/main.py
@@ -4,7 +4,9 @@ from typing import List
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         for i in range(len(matrix) // 2):
+            # Отражаем матрицу относительно прямой X (середины)
             matrix[i], matrix[len(matrix) - i - 1] = matrix[len(matrix) - i - 1], matrix[i]
         for i in range(len(matrix)):
             for j in range(i):
+                # Отражаем матрицу относительно диагонали
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
