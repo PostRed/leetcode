aafdfa1ed37baa771f0481d077ee8b0df15b0fe7
Anаstasiya <102323574+PostRed@users.noreply.github.com>
2024-10-15 13:37:48 +0300

    Update main.py

--- other/roman_to_integer/main.py
+++ other/roman_to_integer/main.py
@@ -1,29 +1,22 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        ans = 0
-        for i in range(len(s)):
-            el = s[i]
-            if el == "M":
-                ans += 1000
-            elif el == "D":
-                ans += 500
-            elif el == "C":
-                if i < len(s) - 1 and s[i + 1] in "DM":
-                    ans -= 100
-                else:
-                    ans += 100
-            elif el == "L":
-                ans += 50
-            elif el == "X":
-                if i < len(s) - 1 and s[i + 1] in "LC":
-                    ans -= 10
-                else:
-                    ans += 10
-            elif el == "V":
-                ans += 5
+        roman_numerals = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total, prev_value = 0, 0
+        for char in reversed(s):
+            value = roman_numerals[char]
+            # для значений типа 4 (IV)
+            if value < prev_value:
+                total -= value
+            # а так - просто складываем
             else:
-                if i < len(s) - 1 and s[i + 1] in "VX":
-                    ans -= 1
-                else:
-                    ans += 1
-        return ans
+                total += value
+            prev_value = value
+        return total
